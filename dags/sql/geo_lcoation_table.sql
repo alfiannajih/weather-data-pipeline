6555f5a7e46bee8696f808ca872382897b40ed33
alfian <alfianhayato@yahoo.co.id>
2024-04-05 08:31:20 +0700

    tasks for creating table sql

---
+++ dags/sql/geo_lcoation_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS geo_location (
+    "id" INTEGER,
+    "province" TEXT,
+    "city" TEXT,
+    "district" TEXT,
+    "longitude" FLOAT,
+    "latitude" FLOAT,
+    PRIMARY KEY ("id")
+);
