abbbe6ff3d7476515e750e675d4ba138944b2bd0
alfian <alfianhayato@yahoo.co.id>
2024-04-08 13:10:36 +0700

    delete file

--- dags/tasks/load_to_sql.py
+++
@@ -1,6 +0,0 @@
-from airflow.providers.postgres.hooks.postgres import PostgresHook
-from airflow.providers.postgres.operators.postgres import PostgresOperator
-
-def load_to_postgres(conn_id, table, file_name):
-    postgres_hook = PostgresHook(postgres_conn_id=conn_id)
-    postgres_hook.bulk_load(table, file_name)
